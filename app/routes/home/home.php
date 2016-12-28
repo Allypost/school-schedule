a6f6eb21e241f7ea1a94c612a3a12ab60fe3a507
Josip Igrec (Allypost) <allypost98@gmail.com>
2016-12-28 02:15:32 +0100

    Added Schedule display

--- app/routes/home/home.php
+++ app/routes/home/home.php
@@ -1,5 +1,17 @@
 <?php
 
 $app->get('/', function () use ($app) {
-    $app->render('home/home.twig');
+    $u = $app->auth;
+
+    if (!$u) {
+        $app->render('home/home.twig');
+
+        return;
+    }
+
+    $classes = $u->schedule();
+    $week    = ((int) (date('W'))) % 2 == 0 ? 'a' : 'b';
+    $days    = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri' ];
+
+    $app->render('home/home.twig', compact('classes', 'week', 'days'));
 });
