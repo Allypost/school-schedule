be31e8c805b326ff1078786b174e2fb69744d618
Josip Igrec (Allypost) <allypost98@gmail.com>
2016-12-30 16:13:22 +0100

    Added Notifications API endpoint

--- app/routes/api/container.php
+++ app/routes/api/container.php
@@ -18,4 +18,8 @@ $app->group('/api', function () use ($app, $loggedIn, $admin, $guest, $cache) {
         require_once 'no-include/lessons/update.php';
     });
 
+    $app->group('/notifications', function () use ($app, $loggedIn, $admin, $guest, $cache) {
+        require_once 'no-include/notifications/list.php';
+    });
+
 });
---
+++ app/routes/api/no-include/notifications/list.php
@@ -0,0 +1,19 @@
+<?php
+
+use Allypost\Lessons\Notification;
+
+$app->get('/count', function () use ($app) {
+    $n = new Notification();
+
+    $count = $n->mine()->count();
+
+    say('notifications list count', compact('count'));
+});
+
+$app->get('/', function () use ($app) {
+    $n = new Notification();
+
+    $data = $n->mine()->get()->toArray();
+
+    say('notifications list', $data);
+})->name('api:notifications:list');
