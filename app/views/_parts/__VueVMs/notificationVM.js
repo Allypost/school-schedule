42c1a9ae02bb0c26ddfe0972364822470678b3f9
Josip Igrec (Allypost) <allypost98@gmail.com>
2017-02-01 01:53:54 +0100

    Moved notificationVM to separate file

---
+++ app/views/_parts/__VueVMs/notificationVM.js
@@ -0,0 +1,34 @@
+var notificationVM = new Vue(
+    {
+        el     : '#notifications',
+        data   : {
+            newer: [],
+            older: []
+        },
+        filters: {
+            ago: function (value) {
+                return moment.utc(value, 'YYYY-MM-DD HH:mm:ss').fromNow();
+            }
+        },
+        methods: {
+            getFetchUrl: function () {
+                return this.$el.dataset.fetchUrl;
+            },
+            fetchAll   : function () {
+                var vm  = this;
+                var url = vm.getFetchUrl();
+
+                $.get(url)
+                 .done(function (data) {
+                     var d = data.data;
+
+                     vm.newer = d.new;
+                     vm.older = d.old;
+                 })
+            }
+        },
+        mounted: function () {
+            this.fetchAll();
+        }
+    }
+);
